7c41ed055f6ce68abb72c1968f365a7121994b02
raj sharma <rs7379364@gmail.com>
2024-08-10 14:30:19 +0530

    main File

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const path  = require('path'); 
+const port = 8000;
+const userRoute  =require('./routes/user');
+const blogRoute = require('./routes/blog');
+const cookieParser = require('cookie-parser');
+const connectWithDB = require('./config/database');
+const { checkForAuthenticationCookie } = require('./middlewares/authentication');
+const Blog= require('./models/blog');
+
+
+// connect mongodb with server
+connectWithDB();
+
+
+// use middleware for handle form data
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(path.resolve('./public')));
+
+app.set('view engine',"ejs");
+app.set('views',path.resolve('./views'));
+// use middleware for parse cookies
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie('token'));
+app.get('/',async (req,res) =>{
+
+    const allBlogs = await Blog.find({});
+    res.render("home",{
+    user:req.user,
+    blogs:allBlogs,
+    })});
+
+
+
+
+
+app.use('/user',userRoute);
+
+app.use('/blog',blogRoute);
+
+app.listen(port,() =>{
+    console.log(`server started at port no.${port}`);
+});
